1851fd1ecac8b26d77f047c71ebd396f5b597bf5
Eugene Huang <eugene.huang99@gmail.com>
2020-06-27 23:05:31 +1000

    Delegate GLibEventLoopPolicy method calls to module functions

--- aioglib/__init__.py
+++ aioglib/__init__.py
@@ -3,15 +3,25 @@ import sys
 from gi.repository import GLib
 
 
+def get_event_loop() -> asyncio.AbstractEventLoop:
+    ...
+
+def set_event_loop(loop: asyncio.AbstractEventLoop) -> None:
+    ...
+
+def new_event_loop() -> asyncio.AbstractEventLoop:
+    ...
+
+
 class GLibEventLoopPolicy(asyncio.AbstractEventLoopPolicy):
     def get_event_loop(self) -> asyncio.AbstractEventLoop:
-        ...
+        return get_event_loop()
 
     def set_event_loop(self, loop: asyncio.AbstractEventLoop) -> None:
-        ...
+        return set_event_loop(loop)
 
     def new_event_loop(self) -> asyncio.AbstractEventLoop:
-        ...
+        return new_event_loop()
 
     if sys.platform != 'win32':
         def get_child_watcher(self) -> asyncio.AbstractChildWatcher:
